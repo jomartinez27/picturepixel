b53de39604ffa5033ebd7d1ee4ab127ee37209f9
Jose Martinez <josmmartinez94@gmail.com>
2018-08-07 16:20:37 -0700

    Add root reducer, session reducer, user reducer, errors reducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,15 +2,22 @@ import * as SessionAPIUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 export const signup = user => dispatch => (
-  SessionAPIUtil.signup(user)
-    .then(user => dispatch(receiveCurrentUser(user)))
+  SessionAPIUtil.signup(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
 )
 
 export const login = user => dispatch => (
-  SessionAPIUtil.login(user)
-    .then(user => dispatch(receiveCurrentUser(user)))
+  SessionAPIUtil.login(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
 )
 
 export const logout = () => dispatch => (
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -10,8 +10,7 @@ const sessionReducer = (oldState = _null_session, action) => {
     case RECEIVE_CURRENT_USER:
       return merge({id : action.user.id});
     case LOGOUT_CURRENT_USER:
-      delete newState[action.user.id];
-      return newState;
+      _null_session;
     default:
       return newState;
   }
