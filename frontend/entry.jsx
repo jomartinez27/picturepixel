33601260ecbb0e66e171998411996b7d8b4b3a6a
Jose Martinez <josmmartinez94@gmail.com>
2018-08-08 11:49:21 -0700

    Bootstrap current user

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -6,9 +6,20 @@ import { login, signup } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  window.login = login;
   ReactDOM.render(<Root store={store}/>, root);
 });
