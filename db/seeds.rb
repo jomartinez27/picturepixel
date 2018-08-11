62d4e1a6aed5aa7af5bbf01190065b82db2e98b7
Jose Martinez <josmmartinez94@gmail.com>
2018-08-11 11:53:02 -0700

    Re-add User.destroy_all

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 ActiveRecord::Base.transaction do
+  User.destroy_all
+
   demo = User.create!(username: 'demo', email: 'demo@demo', password: '123456')
 end
