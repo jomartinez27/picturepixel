b02c226aed52babc8224cec24446164424f08297
Jose Martinez <josmmartinez94@gmail.com>
2019-01-04 08:20:33 -0800

    Add animation to home page

--- frontend/components/home/Home.jsx
+++ frontend/components/home/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { Navbar, Nav, NavItem, Image, Grid, Row, Col, Button, Jumbotron } from 'react-bootstrap';
+import { TimelineMax } from 'gsap/all'
 
 class Home extends React.Component {
+  componentDidMount() {
+    let bg = document.getElementById('home-splash');
+    let tl = new TimelineMax();
+    tl.from(bg, 2, {opacity:0, y:-100})
+    tl.from(".demo-tag", 1, {x:-100, opacity:0})
+    tl.from([".demo-text", "#home-button"], 2, {opacity:0, y:-100})
+  }
+
   render () {
     return (
       <div className="home-splash">
-        <Image src={window.images.la} responsive className="home-splash"/>
+        <Image src={window.images.la} responsive className="home-splash" id="home-splash"/>
         <Navbar inverse collapseOnSelect className="home-nav">
           <Navbar.Header>
             <Navbar.Brand>
@@ -32,7 +41,7 @@ class Home extends React.Component {
           <Jumbotron className="jumbo-home">
             <h2 className="demo-tag">Experience the world in Pixelated Art</h2>
             <h3 className="demo-text">Discover images by other users</h3>
-            <Button className="home-button" href="#/login">Demo User</Button>
+            <Button className="home-button" href="#/login" id="home-button">Demo User</Button>
           </Jumbotron>
         </div>
       </div>
